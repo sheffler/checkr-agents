a49d8505af0a2c419dd40746c460200a59791e13
Thomas Sheffler <tom.sheffler@gmail.com>
2025-10-25 09:11:44 -0700

    populate some example agents

---
+++ checkr_agents/agents/nlip_agent.py
@@ -0,0 +1,79 @@
+#
+# An NLIP Agent knows how to respond to the request for NLIP Capabilities
+#
+
+import asyncio
+import logging
+from typing import Callable
+
+from .checkr_agent import CheckrAgent
+from .checkr_agent import MODEL
+
+
+logger = logging.getLogger("NLIP")
+
+NLIP_INSTRUCTION = """
+You are an NLIP Agent.
+
+NLIP is an acronym for "Natural Language Interaction Protocol."  The NLIP project aims to define a protocol for the following use cases.
+- Agent to Agent interactions in Natural Language
+- User-Agent to Agent protocol
+
+An NLIP Agent, when defined, is given a system instruction that describes its unique capabilities.
+
+One of the first requests an NLIP Agent will be asked to fulfill is to describe its NLIP Capabilities.
+When you are asked to describe your NLIP Capabilities, you should respond with a response of the format:
+    AGENT:NAME
+    CAPABILITY1:description, CAPABILITY2:description, CAPABILITY3:description, ...
+- where NAME is your name
+- CAPABILITITY1, CAPABILITY2 and CAPABILITY3 are dictionary keys.  The description associated with each should be unique.
+- it is important that you include your NAME
+- it is important that the capabilities are described in a means that another LLM can understand them
+
+"""
+
+
+class NlipAgent(CheckrAgent):
+
+    def __init__(self,
+                 name: str,
+                 model: str = MODEL,
+                 instruction: str = None,
+                 tools: list[Callable] = [ ]
+                 ):
+
+        super().__init__(name, model, NLIP_INSTRUCTION, tools)
+
+        # any additional instructions
+        if instruction:
+            self.add_instruction(instruction)
+
+
+
+
+#
+# Test program for trying out a simple agent with the console
+#
+
+async def main():
+    logger.info(f"WELCOME")
+    if len(sys.argv) < 0:
+        print("Usage: python client.py <path_to_server_script>")
+        sys.exit(1)
+
+    agent = NlipAgent(
+        name="Bart"
+    )
+    
+    try:
+        await agent.chat_loop()
+    finally:
+        await agent.cleanup()
+
+
+if __name__ == "__main__":
+    import sys
+    import checkr_agents
+    checkr_agents.log_to_console(logging.DEBUG)
+
+    asyncio.run(main())
---
+++ checkr_agents/agents/rag_nlip_agent.py
@@ -0,0 +1,111 @@
+#
+# A Weather Agent has tools for querying about weather conditions and alerts.
+# An additional system instruction helps refine its behavior.
+
+import logging
+import asyncio
+from importlib import resources
+import os
+
+from .nlip_agent import NlipAgent
+from .checkr_agent import MODEL
+from ..rag.milvus_context_generator import MilvusContextGenerator
+
+# use the default logger
+from checkr_agents import logger
+
+NLIP_INSTRUCTION = """You are an agent with specialized knowledge about the McLaren Labs website stored
+in a vector database that is used to provide context to queries.
+You know about RTP-MIDI and MIDI in general.  You know about Objective-C programming,
+and how to access MIDI and Audio devices in LInux.
+"""
+
+DB = "rag_website_milvus.db" # in the /resources directory
+EMBEDDING_MODEL = 'ollama/granite-embedding:30m'
+EMBEDDING_LENGTH = 384
+COLLECTION_NAME = 'pages'
+
+#
+# A helper function to locate a milvus file in checker_agents/resources/foo
+# and turn it into a relative path.
+#
+# Milvus is not ok with absolute paths to the .db file.  It wants a path relative
+# to the current working dir.
+#
+
+def find_db_path_in_resources(db:str):   
+    posix_path = resources.files('checkr_agents.resources').joinpath('rag_website_milvus.db')
+    rel_path = posix_path.relative_to(os.getcwd())
+    logger.info(f"MILVUS DB located in {rel_path}")
+    return str(rel_path)
+
+class RagNlipAgent(NlipAgent):
+
+    def __init__(self,
+                 name: str,
+                 model: str = MODEL,
+                 instruction: str = None,
+                 ):
+
+        super().__init__(name, model=model)
+
+        # initialize the vector database context generator
+        URI = find_db_path_in_resources(DB)
+        self.mcg = MilvusContextGenerator(URI, EMBEDDING_MODEL, EMBEDDING_LENGTH, COLLECTION_NAME)
+
+        self.add_instruction(NLIP_INSTRUCTION)
+
+        if instruction:
+            self.add_instruction(instruction)
+
+    #
+    # Override the query function to provide context from Milvus database
+    #
+
+    async def process_query(self, query: str) -> list[str]:
+
+        chunks = self.mcg.retrieve_context_from_milvus(query)
+
+        context_str = "\n---\n".join(chunks)
+
+        system_prompt = (
+            "You are an expert Q&A assistant. Use the provided context "
+            "to answer the user's question. If the answer is not in the context, "
+            "state that you cannot find the answer in the provided documents."
+        )
+
+        user_prompt = (
+            f"Context: {context_str}\n\n"
+            f"Question: {query}"
+        )
+
+        self.add_instruction(system_prompt)
+        return await super().process_query(user_prompt)
+        
+    
+
+# Test program for running with stdin
+async def main():
+    logger.info(f"WELCOME")
+    if len(sys.argv) < 0:
+        print("Usage: python client.py <path_to_server_script>")
+        sys.exit(1)
+
+    agent = RagNlipAgent(
+        "McLarenLabs"
+    )
+    
+    # agent.load_spec("checkr_agents.assertions.assertion1:mainfn")
+
+    try:
+        await agent.chat_loop()
+    finally:
+        await agent.cleanup()
+
+
+if __name__ == "__main__":
+    import sys
+    import checkr_agents
+    checkr_agents.log_to_console(logging.DEBUG)
+
+    asyncio.run(main())
