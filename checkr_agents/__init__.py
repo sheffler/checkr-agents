6b40c3e5b53b006e2e9fa2ffcc7cf4594ec9beab
Thomas Sheffler <tom.sheffler@gmail.com>
2025-10-25 09:10:40 -0700

    update logging

--- checkr_agents/__init__.py
+++ checkr_agents/__init__.py
@@ -2,5 +2,9 @@
 # Include some simple logging configurators
 from checkr_agents._logging import (
     logger,
-    log_to_console
+    log_to_console,
+    assertion_logger
 )
+
+# global table of registered apps for the mem://name protocol
+MEM_APP_TBL = { }
--- checkr_agents/_logging.py
+++ checkr_agents/_logging.py
@@ -13,3 +13,23 @@ def log_to_console(level):
     console_handler.setFormatter(formatter)
     logger.addHandler(console_handler)
     
+
+#
+# A function that Assertions can use to set up their logger in a consistent way
+#   ex: logger = checkr_agents.assertion_logger("assertion1")
+#
+
+def assertion_logger(name: str):
+
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+
+    file_handler = logging.FileHandler(f"{name}.log")
+    file_handler.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
+    return logger
+
