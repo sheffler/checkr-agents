25ce7830725fa92aa4e3aa04050d9caa932ebdcf
Thomas Sheffler <tom.sheffler@gmail.com>
2025-10-25 13:55:09 -0700

    http_server for checkr-clients.  Exposes NLIP on a standard port.

---
+++ checkr_agents/http_server/README.md
@@ -0,0 +1,26 @@
+# NLIP Session Server
+
+Checkr Agents all use HTTP for inter-agent communication and expose their input channel over a standard NLIP endpoint.
+
+This is a straightforward FastAPI server that can receive and send NLIP Messages.  It implements an Unauthenticated Session store so that multiple conversations can be handled by different instances of an Agent.
+
+
+
+## Authentication
+
+This server does not currently implement any form of authentication.
+
+## Sessions
+
+This server implements a very simple in-memory Session store.  An HTTP Cookie in the client is used to track the Session data.  The Cookie name is configurable.
+
+## The Docs View
+
+FastAPI provides an OpenAPI docs viewer that can be used to interact with the server.  For instance, the Weather Server is normally launched on port 8022, and its docs view can be accessed at the URL shown below.  The NLIP Session Server provides an example minimal NLIP message where you can modify the "content" field and "Execute" a request.  Because there is a session, the conversation history is remembered across requests.
+
+- [http://localhost:8020/docs](http://localhost:8020/docs)  (The Basic Agent)
+- [http://localhost:8022/docs](http://localhost:8022/docs)  (The Weather Agent)
+- [http://localhost:8024/docs](http://localhost:8024/docs)  (The NLIP Coordinator Agent)
+
+![The Docs View](./pics/FastAPIDocsView-01.png)
+
---
+++ checkr_agents/http_server/nlip_session_server.py
@@ -0,0 +1,155 @@
+#
+# An HTTP Server with session management.
+#
+
+from fastapi import FastAPI, Body, Request, Response, Depends, HTTPException
+from typing import Dict, Annotated
+from uuid import uuid4
+import traceback
+import sys
+
+from nlip_sdk.nlip import NLIP_Message
+
+#
+# Session-oriented NLIP handler
+#
+
+class SessionManager:
+
+    async def process_nlip(self, msg: NLIP_Message) -> NLIP_Message:
+        raise NotImplementedError("Subclasses must implement this method")
+    
+#
+# A fast api server with two predefined routes
+class NlipSessionServer(FastAPI):
+
+    def __init__(self, suffix:str, session_manager_class):
+
+        super().__init__()
+
+        self.suffix = suffix
+        self.session_manager_class = session_manager_class
+
+        self.session_cookie_name = f"session_id_{suffix}"
+        self.sessions: Dict[str, SessionManager] = { }
+
+        app = self
+
+        @app.post("/nlip")
+        async def process_nlip_request(
+                message: Annotated[NLIP_Message, Body(examples=examples)],
+                manager: SessionManager = Depends(self.get_session_manager)
+        ):
+            try:
+                response = await manager.process_nlip(message)
+                return response
+            except Exception as e:
+                print(f"NlipSessionServer:{e}")
+                traceback.print_exc(file=sys.stdout)
+                raise HTTPException(status_code=400, detail=str(e))
+
+
+        @app.get("/health")
+        async def health_check():
+            return {"status": "healthy"}
+        
+    # A dependency function to get or create a session
+    def get_session_manager(self, request: Request, response: Response) -> SessionManager:
+        """
+        Retrieves the Session manager instance associated with the current session.
+        A new session and manager are created if not exists.
+        """
+
+        session_id = request.cookies.get(self.session_cookie_name)
+
+        if not session_id or session_id not in self.sessions:
+            # Create a new session and manager
+            session_id = str(uuid4())
+            self.sessions[session_id] = self.session_manager_class()
+
+            # Set cookie to remember this session
+            response.set_cookie(
+                key=self.session_cookie_name,
+                value=session_id,
+                httponly=True,
+                samesite="lax",
+            )
+            print(f"Created new session and agent for session_id: {session_id}")
+
+        return self.sessions[session_id]
+
+# #
+# # CONFIGURATION VARS to be set at runtime
+# #
+
+# SESSION_MANAGER_CLASS = SessionManager
+# SESSION_COOKIE_NAME = "session_id"
+
+# # In-memory store for sessions.  In a production app this would be a database or cache.
+# sessions: Dict[str, SessionManager] = { }
+
+# app = FastAPI()
+
+# # A dependency function to get or create a session
+# def get_session_manager(request: Request, response: Response) -> SessionManager:
+#     """
+#     Retrieves the Session manager instance associated with the current session.
+#     A new session and manager are created if not exists.
+#     """
+
+#     session_id = request.cookies.get(SESSION_COOKIE_NAME)
+
+#     if not session_id or session_id not in sessions:
+#         # Create a new session and manager
+#         session_id = str(uuid4())
+#         sessions[session_id] = SESSION_MANAGER_CLASS()
+
+#         # Set cookie to remember this session
+#         response.set_cookie(
+#             key=SESSION_COOKIE_NAME,
+#             value=session_id,
+#             httponly=True,
+#             samesite="lax",
+#         )
+#         print(f"Created new session and agent for session_id: {session_id}")
+
+#     return sessions[session_id]
+
+
+# # Provide examples of simple NLIP messages that are displayed in the docs UI
+examples = [
+    {
+        "format": "text",
+        "subformat":"english",
+        "content": "How are you today?"
+    },
+    {
+        "format": "text",
+        "subformat":"english",
+        "content": "Is there a weather alert for CA?"
+    },
+    {
+        "format": "text",
+        "subformat":"english",
+        "content": "Describe your NLIP Capabilities."
+    },
+]
+
+
+# @app.post("/nlip")
+# async def process_nlip_request(
+#         message: Annotated[NLIP_Message, Body(examples=examples)],
+#         manager: SessionManager = Depends(get_session_manager)
+#         ):
+#     try:
+#         response = await manager.process_nlip(message)
+#         return response
+#     except Exception as e:
+#         print(e)
+#         traceback.print_exc(file=sys.stdout)
+#         raise HTTPException(status_code=400, detail=str(e))
+
+
+# @app.get("/health")
+# async def health_check():
+#     return {"status": "healthy"}
